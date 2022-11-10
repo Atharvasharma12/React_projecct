e35b395693661c08c4a9f4fcafd002737bf2d015
Atharva sharma <89251910+Atharvasharma12@users.noreply.github.com>
2022-11-10 21:17:24 +0530

    photos directory changed

--- src/HomeSection.jsx
+++ src/HomeSection.jsx
@@ -8,9 +8,9 @@ function HomeSection() {
     
     <section className='home'>
     <div className='standParent'>
-
       <img src={Standimage} className="stand" alt=""></img>
     </div>
+      <img src="book1.jpg"  className="stand_book1" alt=""></img>
 
     </section>
   )
